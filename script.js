98c3ac0fc178bd369dbb9744c968fe997f2e73be
Lopesllm <55158285+Lopesllm@users.noreply.github.com>
2022-08-29 20:35:18 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 let date = new Date();
 let options = {weekday: 'long', year: 'numeric', 
-                month: 'long', day: 'numeric'
+                month: 'long', day: 'numeric',
                 hour: 'numeric', minute: 'numeric', second: 'numeric'};
 document.write(date.toLocaleDateString('pt-BR', options))
 document.write(date.toLocaleDateString('en-US', options));
