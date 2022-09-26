4dea5a9497509d4e077fb4496f6a1298b121b39d
Lopesllm <55158285+Lopesllm@users.noreply.github.com>
2022-09-26 20:00:44 -0300

    Update scriptDate.js

--- scriptDate.js
+++ scriptDate.js
@@ -2,9 +2,7 @@ let date = new Date();
 let options = {weekday: 'long', year: 'numeric', 
                 month: 'long', day: 'numeric',
                 hour: 'numeric', minute: 'numeric', second: 'numeric'};
-document.write(date.toLocaleDateString('pt-BR', options))
-
-document.write(date.toLocaleDateString('en-US', options));
+document.write(date.toLocaleDateString('pt-BR', options));
 /*Referência
 https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/
 Reference/Global_Objects/Date/toLocaleDateString */
